6334b7e0bf8ea34b4cef50172b96311f1af23831
Emmanuel Abaye <abayeemmanuel@yahoo.com>
2017-11-21 16:31:20 +0100

    Chore: Added tests for some API endpoints

--- server/test/test.js
+++ server/test/test.js
@@ -2,6 +2,8 @@ import chai from 'chai';
 import chaiHttp from 'chai-http';
 import app from '../server.js';
 
+console.log("ENV ", process.env.NODE_ENV);
+
 chai.use(chaiHttp);
 
 const expect = chai.expect;
@@ -28,5 +30,79 @@ describe('API endpoints /api/v1', () => {
       }),
   );
 
+
+
+});
+
+
+describe('API endpoints /api/v1/', () => {
+
+
+  it(
+    'Should create new user',
+    () => chai.request(app)
+      .post('/api/v1/users')
+      .send({
+        firstName: 'AdminName',
+        lastName: 'AdminSurname',
+        email: 'user' + Math.random() + '@user.com',
+        password: 'password'
+      })
+      .then((res) => {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+      }),
+  );
+
+
+  // POST - should pass authentication
+  it(
+    'Should pass authentication',
+    () => chai.request(app)
+      .post('/api/v1/users/login')
+      .send({email: 'Admin@Admin.com', password:'password'})
+      .then((res) => {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+      }),
+  );
+
+  it(
+    'Should sign out, return status 200',
+    () => chai.request(app)
+        .get('/api/v1/users/logout')
+        .then((res) => {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+      }),
+  );
+
+  // POST - should return status 200
+  it(
+    'Should return status 200',
+    () => chai.request(app)
+      .get('/api/v1/centers')
+      .then((res) => {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+      }),
+  );
+
+   // POST - should return status 200
+  it(
+    'Should return status 200',
+    () => chai.request(app)
+      .get('/api/v1/centers')
+      .then((res) => {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+      }),
+  );
+
+
+  
+
+
+ 
 
 });
