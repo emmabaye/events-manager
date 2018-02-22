56aa15f9918ec79b49d853ad1585bba769c8d7d2
Emmanuel Abaye <abayeemmanuel@yahoo.com>
2018-02-22 13:22:13 +0100

    Ch:e2e tests for modify and delete event

--- e2e/tests/events.js
+++ e2e/tests/events.js
@@ -7,13 +7,12 @@ export default {
       .setValue('input[name=password]', client.globals.env.ADMIN_PASSWORD)
       .click('form button[type=submit]')
       .waitForElementVisible('body', 2000)
-      .pause(2000)
+      .pause(5000)
       .verify.urlEquals(`${client.launch_url}/myevents`)
       .assert.elementPresent(".navbar a[href='/addevent']");
   },
 
   'add event': (client) => {
-    console.log("DATE ",client.globals.event.date)
     client
       .click("a[href='/addevent']")
       .assert.urlEquals(`${client.launch_url}/addevent`)
@@ -26,7 +25,7 @@ export default {
       .setValue('input[name=image]', client.globals.event.image)
       .click('form button[type=submit]')
       .waitForElementVisible('body', 2000)
-      .pause(2000)
+      .pause(5000)
       .verify.urlEquals(`${client.launch_url}/myevents`)
   },
 
@@ -37,15 +36,31 @@ export default {
       .assert.visible(".event-details")
       .assert.visible(".venue-date")
       .assert.visible(".card .mapouter")
-      .pause(3000)
       .click("a[href='/myevents']")
+      .pause(5000)
       .verify.urlEquals(`${client.launch_url}/myevents`)
   },
 
-  'modify event': () => {
+  'modify event': (client) => {
+    client
+      .waitForElementVisible(".event:last-child .btn-success", 5000)
+      .click(".event:last-child .btn-success")
+      .pause(5000)
+      .verify.urlContains(`${client.launch_url}/modifyevent/`)
+      .assert.visible('form')
+      .setValue('input[name=title]', 'Birthday party')
+      .click('form button[type=submit]')
+      .waitForElementVisible('body', 2000)
+      .pause(5000)
+      .verify.urlEquals(`${client.launch_url}/myevents`)
   },
 
-  'delete event': () => {
-
+  'delete event': (client) => {
+    client
+      .waitForElementPresent(".event:last-child a.btn-danger", 5000)
+      .click(".event:last-child a.btn-danger")
+      .pause(5000)
+      .waitForElementVisible(".event:last-child .modal button.btn-danger", 5000)
+      .click(".event:last-child .modal button.btn-danger")
   }
 };
