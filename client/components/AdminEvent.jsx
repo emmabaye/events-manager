fdf34dabd3096d4c1c24ba10b1be1cf1e618f2af
Emmanuel Abaye <abayeemmanuel@yahoo.com>
2018-03-13 15:29:29 +0100

    ch: fixed lint errors for AdminEvent component

--- client/components/AdminEvent.jsx
+++ client/components/AdminEvent.jsx
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import moment from 'moment';
 
+/**
+ * React component for admin event
+ * card displayed in the admin panel
+ */
 class AdminEvent extends Component {
-  constructor(props) {
-    super(props);
-  }
-
+  /**
+   * React's method to render react component
+   * @return {object}
+   */
   render() {
     return (
-			 <div className="card-block">
+      <div className="card-block">
         <div className="container events">
           <div className="row event-row">
             <div className="col-md-4 event">
@@ -20,7 +24,10 @@ class AdminEvent extends Component {
                   <p className="card-text h6"><b>Date</b>: {moment(this.props.event.date).format('DD MMMM YYYY')}</p>
                   <p className="card-text h6"><b>Time</b>: { this.props.event.time }</p>
                   <p className="card-text h6"><b>Status</b>: Holding</p>
-                  <p className="text-center"><a href={`/events/${this.props.event.id}`} className="btn btn-sm btn-primary"><i className="fa fa-info-circle fa-lg" /> View Event</a>
+                  <p className="text-center">
+                    <a href={`/events/${this.props.event.id}`} className="btn btn-sm btn-primary">
+                      <i className="fa fa-info-circle fa-lg" /> View Event
+                    </a>
                     <a href="#" className="btn btn-sm btn-danger" ><i className="fa fa-times fa-lg" /> Cancel</a></p>
                 </div>
               </div>
