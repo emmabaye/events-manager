bc8b78c3b613ad851970bd3c9feaec22389a7ad1
Emmanuel Abaye <abayeemmanuel@yahoo.com>
2018-03-13 16:34:28 +0100

    ch: fixed lint errors for AdminPanelBody component

--- client/components/AdminPanelBody.jsx
+++ client/components/AdminPanelBody.jsx
@@ -1,10 +1,19 @@
+/* eslint-disable import/no-named-as-default */
 import React, { Component } from 'react';
 import AdminCenters from './AdminCenters.jsx';
 import AdminCenterDetails from './AdminCenterDetails.jsx';
 import AddCenter from './AddCenter.jsx';
 import ModifyCenter from './ModifyCenter.jsx';
 
+/**
+ * React component for admin panel body
+ */
 class AdminPanelBody extends Component {
+  /**
+   * Constructor for AdminPanelBody Component
+   * @param  {objects} props React component props
+   * @return {undefined}
+   */
   constructor(props) {
     super(props);
     this.state = {
@@ -25,6 +34,12 @@ class AdminPanelBody extends Component {
     };
   }
 
+  /**
+   * Show and hides components
+   * @param  {[type]} elem     component to show or hide
+   * @param  {[type]} centerId center id to get details of a specific center
+   * @return {undefined}
+   */
   show = (elem, centerId) => {
     let hiddenElems = {
       centers: { visible: false },
@@ -39,15 +54,21 @@ class AdminPanelBody extends Component {
     });
   }
 
-
+  /**
+   * React's method to render react component.
+   *
+   * @return {object}
+   */
   render() {
     return (
       <div className="container-fluid admin">
         <div className="row">
           <div className="col-sm-2 side">
             <div className="list-group sidebar">centerId
-              <a href="#centers" className="list-group-item list-group-item-action centers-button" onClick={() => this.show('centers')}>Centers</a>
-              <a href="#add-center" className="list-group-item list-group-item-action add-center-button" onClick={() => this.show('addCenter')}>Add Center</a>
+              <a href="#centers" className="list-group-item list-group-item-action centers-button"
+                onClick={() => this.show('centers')}>Centers</a>
+              <a href="#add-center" className="list-group-item list-group-item-action add-center-button"
+                onClick={() => this.show('addCenter')}>Add Center</a>
               <a href="#events" className="list-group-item list-group-item-action" >Events</a>
               <a href="#users" className="list-group-item list-group-item-action">Users</a>
             </div>
@@ -55,8 +76,12 @@ class AdminPanelBody extends Component {
           <div className="col-sm-10 panel-column">
             { (this.state.centers.visible) && <AdminCenters show={this.show} /> }
             { (this.state.addCenter.visible) && <AddCenter show={this.show} /> }
-            { (this.state.modifyCenter.visible) && <ModifyCenter show={this.show} centerId={this.state.modifyCenter.centerId} /> }
-            { (this.state.centerDetails.visible) && <AdminCenterDetails centerId={this.state.centerDetails.centerId} /> }
+            { (this.state.modifyCenter.visible) &&
+              <ModifyCenter show={this.show} centerId={this.state.modifyCenter.centerId} />
+            }
+            { (this.state.centerDetails.visible) &&
+              <AdminCenterDetails centerId={this.state.centerDetails.centerId} />
+            }
           </div>
         </div>
       </div>
