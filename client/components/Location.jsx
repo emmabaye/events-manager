03a7e86c0a5708f626a74278dd8cbd76908ef56f
Emmanuel Abaye <abayeemmanuel@yahoo.com>
2018-03-15 12:01:23 +0100

    ch: fixed linter errors for Location component

--- client/components/Location.jsx
+++ client/components/Location.jsx
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
 
+/**
+ * React  component for location of an event
+ * or a center using google map
+ */
 class Location extends Component {
-  constructor(props) {
-    super(props);
-  }
-
+  /**
+   * React's method to render react component.
+   * Renders google map
+   *
+   * @return {object}
+   */
   render() {
     return (
       <div className="card" style={{ minHeight: "350px" }}>
         <h6 className="card-header"><b>LOCATION</b></h6>
         <div className="card-block">
-          <div className="mapouter" style={{ overflow: "hidden", height: "100%", width: "100%" }}><div className="gmap_canvas" style={{ background: "none", height: "100%", width: "100%" }}>
-					  <iframe style={{ width: "100%", minHeight: "400px" }} id="gmap_canvas" src={`https://maps.google.com/maps?q=${this.props.location}&t=&z=9&ie=UTF8&iwloc=&output=embed`} frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" />
+          <div className="mapouter" style={{ overflow: "hidden", height: "100%", width: "100%" }}>
+            <div className="gmap_canvas" style={{ background: "none", height: "100%", width: "100%" }}>
+              <iframe style={{ width: "100%", minHeight: "400px" }} id="gmap_canvas" src={`https://maps.google.com/maps?q=${this.props.location}&t=&z=9&ie=UTF8&iwloc=&output=embed`} frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" />
+            </div>
           </div>
-          {/* <style>.mapouter{overflow:hidden;height:100%;width:100%;}.gmap_canvas {background:none!important;height:100%;width:100%;}</style> */}</div>
         </div>
       </div>
     );
