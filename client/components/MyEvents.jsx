f9028f9e933f1684e5c650a47167c6e188e8a7ea
Emmanuel Abaye <abayeemmanuel@yahoo.com>
2018-03-15 23:34:11 +0100

    ch: Fixed linter errors in MyEvents Component

--- client/components/MyEvents.jsx
+++ client/components/MyEvents.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import axios from 'axios';
 import jwtDecode from 'jwt-decode';
 import NavBar from './NavBar.jsx';
 import Event from './Event.jsx';
-import DeleteModal from './DeleteModal.jsx';
-import { connect } from 'react-redux';
 import { history } from '../routes';
 
+/**
+ * React  component for dispplay of authenticated
+ * user's events
+ */
 export class MyEvents extends Component {
+  /**
+   * Constructor
+   * @param {object} props
+   * @returns {undefined}
+   */
   constructor(props) {
     super(props);
     this.state = {
@@ -15,6 +23,12 @@ export class MyEvents extends Component {
     };
   }
 
+  /**
+   * Check if user is logged in.
+   * If jwt has expired, user is not logged in.
+   *
+   * @return {undefined}
+   */
   componentWillMount() {
     let token = localStorage.getItem('x-access-token');
     try {
@@ -28,27 +42,12 @@ export class MyEvents extends Component {
     }
   }
 
-  componentDidUpdate() {
-    let token = localStorage.getItem('x-access-token');
-    let decoded = jwtDecode(token);
-    let userId = decoded.id;
-
-    axios({
-      method: 'GET',
-      url: `/api/v1/users/${userId}`,
-      withCredentials: true,
-    })
-      .then((response) => {
-        if (this.state.myEvents.length > response.data.data.Events.length) {
-          this.setState({ myEvents: response.data.data.Events });
-        }
-      })
-      .catch((err) => {
-        console.log(err.response);
-      });
-  }
-
-
+  /**
+   * Make style changes after component has mounted.
+   * Gets user's event
+   *
+   * @return {undefined}
+   */
   componentDidMount() {
     document.body.title = 'My Events | Events Manager';
     document.body.style.backgroundImage = "url('../img/ambitious-creative-co-rick-barrett-110145.jpg')";
@@ -59,6 +58,7 @@ export class MyEvents extends Component {
     let token = localStorage.getItem('x-access-token');
 
     try {
+      //eslint-disable-next-line no-unused-vars
       let decoded = jwtDecode(token);
     } catch (e) {
       return history.push("/login");
@@ -79,23 +79,60 @@ export class MyEvents extends Component {
       });
   }
 
+  /**
+   * Updates component state
+   *
+   * @return {undefined}
+   */
+  componentDidUpdate() {
+    let token = localStorage.getItem('x-access-token');
+    let decoded = jwtDecode(token);
+    let userId = decoded.id;
 
+    axios({
+      method: 'GET',
+      url: `/api/v1/users/${userId}`,
+      withCredentials: true,
+    })
+      .then((response) => {
+        if (this.state.myEvents.length > response.data.data.Events.length) {
+          this.setState({ myEvents: response.data.data.Events });
+        }
+      })
+      .catch((err) => {
+        console.log(err.response);
+      });
+  }
+
+  /**
+   * React's method to render react component.
+   * Renders all of user's event
+   *
+   * @return {object}
+   */
   render() {
     return (
       <div>
         <NavBar page="MyEvents" auth/>
         <div className="container events">
-			            <div className="row event-row">
+          <div className="row event-row">
             {
               (this.state.myEvents.map((event) => <Event key={event.id} eventDetails={event} />))
             }
-			            </div>
-		            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
+/**
+ * Makes the necessary  redux state available in this
+ * component's props
+ *
+ * @param  {object} state global state
+ * @return {object} props object
+ */
 const mapStateToProps = (state) => ({
   allState: state.userReducer
 });
