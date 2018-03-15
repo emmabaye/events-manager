4ff337cd1f93a9220811f5c75aac5c5fdf1d8ecd
Emmanuel Abaye <abayeemmanuel@yahoo.com>
2018-03-15 23:51:53 +0100

    ch: Fixed linter errors in SignOut Component

--- client/components/SignOut.jsx
+++ client/components/SignOut.jsx
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
-import { history } from '../routes';
 import { Redirect } from 'react-router-dom';
 
+/**
+ * React  component for Signing out
+ */
 class SignOut extends Component {
+  /**
+   * Logs out user by replacing jwt with empty
+   * string in localStorage.
+   *
+   * @return {undefined}
+   */
   componentWillMount() {
     localStorage.setItem('x-access-token', "");
   }
 
+  /**
+   * React's method to render react component.
+   * Redirects to index page
+   *
+   * @return {object}
+   */
   render() {
     return (
       <Redirect to="/" push />
