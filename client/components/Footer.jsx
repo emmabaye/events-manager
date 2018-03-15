bea78bf58311e72666674c8f21b1a16308e3e5ab
Emmanuel Abaye <abayeemmanuel@yahoo.com>
2018-03-15 11:52:34 +0100

    ch: fixed linter errors for Footer component

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 
+/**
+ * React  component for footer
+ */
 export default class Footer extends Component {
-  render() {
+  /**
+   * React's method to render react component.
+   * Renders footer
+   *
+   * @return {object}
+   */
+  render() { //eslint-disable-line class-methods-use-this
     return (
       <footer className="container-fluid">
         <p className="h6 text-center"><i className="fa fa-copyright" /> 2017 EVENTS MANAGER</p>
