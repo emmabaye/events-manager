5cc1731b50ad0a64f7332bab54a603fc391492bd
Emmanuel Abaye <abayeemmanuel@yahoo.com>
2018-03-16 12:58:28 +0100

    ch: Fixed linter errors

--- client/index.js
+++ client/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Routes from './routes.js'
 import { Provider } from 'react-redux';
+import Routes from './routes';
 import store from './store';
-import App from './containers/App'
 
 
 ReactDOM.render(
-<Provider store={store} >
- <Routes />
- </Provider>,
-document.getElementById('app')
-)
+  <Provider store={store} >
+    <Routes />
+  </Provider>,
+  document.getElementById('app')
+);
