f7a8ae3492fa7442ba4f894f170ec8f1c371020c
Emmanuel Abaye <abayeemmanuel@yahoo.com>
2018-03-16 13:04:33 +0100

    ch: Fixed linter errors

--- client/routes.js
+++ client/routes.js
@@ -1,8 +1,9 @@
+/* eslint-disable import/no-named-as-default */
 import React from 'react';
-import { BrowserRouter, Router, Route, Switch, Link } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import { createBrowserHistory, createMemoryHistory } from 'history';
-import MyEvents from './components/MyEvents.jsx'
-import App from './containers/App.js';
+import MyEvents from './components/MyEvents.jsx';
+import App from './containers/App';
 import SignUpForm from './components/SignUpForm.jsx';
 import SignInForm from './components/SignInForm.jsx';
 import AllCenters from './components/AllCenters.jsx';
@@ -16,22 +17,20 @@ import SignOut from './components/SignOut.jsx';
 
 export const history = (process.env.NODE_ENV === 'test') ? createMemoryHistory() : createBrowserHistory();
 
-export default () => {
- return (
-   <Router history={history}>
-     <Switch>
-      <Route exact path='/' component={App}/>
-      <Route exact path='/signup' component={SignUpForm}/>
-      <Route exact path='/login' component={SignInForm}/>
-      <Route exact path='/myevents' component={MyEvents}/>
-      <Route exact path='/centers' component={AllCenters}/>
-      <Route exact path='/centers/:id' component={CenterDetails}/>
-      <Route exact path='/addevent' component={AddEvent}/>
-      <Route exact path='/events/:id' component={EventDetails}/>
-      <Route path='/modifyevent/:id' component={ModifyEvent}/>
-      <Route exact path='/admin' component={AdminPanel}/>
-      <Route exaxt path='/logout' component={SignOut} />
-     </Switch>
-   </Router>
- )
-}
+export default () => (
+  <Router history={history}>
+    <Switch>
+      <Route exact path="/" component={App}/>
+      <Route exact path="/signup" component={SignUpForm}/>
+      <Route exact path="/login" component={SignInForm}/>
+      <Route exact path="/myevents" component={MyEvents}/>
+      <Route exact path="/centers" component={AllCenters}/>
+      <Route exact path="/centers/:id" component={CenterDetails}/>
+      <Route exact path="/addevent" component={AddEvent}/>
+      <Route exact path="/events/:id" component={EventDetails}/>
+      <Route path="/modifyevent/:id" component={ModifyEvent}/>
+      <Route exact path="/admin" component={AdminPanel}/>
+      <Route exaxt path="/logout" component={SignOut} />
+    </Switch>
+  </Router>
+);
