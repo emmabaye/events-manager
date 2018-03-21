da42259ff87cefecf7424c78b8ead558c49e1fb7
Emmanuel Abaye <abayeemmanuel@yahoo.com>
2018-03-21 23:27:11 +0100

    Fixed linter error

--- client/__tests__/Body.test.js
+++ client/__tests__/Body.test.js
@@ -3,5 +3,6 @@ import Body from '../components/Body.jsx';
 describe('Body of Landing page should render self', () => {
   it('it should render for body', () => {
     const { wrapper } = shallow(<Body />);
+    expect(wrapper.length).toEqual(1);
   });
 });
