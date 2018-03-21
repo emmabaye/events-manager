f3e94834494d888526cd2238bc7191b65199a8da
Emmanuel Abaye <abayeemmanuel@yahoo.com>
2018-03-22 00:00:53 +0100

    Fixed linter error

--- client/__tests__/LandingPage.test.js
+++ client/__tests__/LandingPage.test.js
@@ -3,5 +3,6 @@ import LandingPage from '../components/LandingPage.jsx';
 describe('Landing Page Component', () => {
   it('it should render landing page', () => {
     const { wrapper } = shallow(<LandingPage />);
+    expect(wrapper.length).toEqual(1);
   });
 });
