3fc70e599061c83f4ddf65319855f9f120d39af7
Emmanuel Abaye <abayeemmanuel@yahoo.com>
2018-03-22 00:22:12 +0100

    Fixed linter errors

--- client/__tests__/SignOut.test.jsx
+++ client/__tests__/SignOut.test.jsx
@@ -9,6 +9,7 @@ describe('Sign out Component', () => {
   describe('Sign out component should render self', () => {
     it('should render self', () => {
       const wrapper = shallow(<SignOut />);
+      expect(wrapper.length).toEqual(1);
     });
   });
 });
